cb754b0c6e4fb94b55edbd45164622a3163cc498
HansFeddersen <97700014+HansFeddersen@users.noreply.github.com>
2022-03-13 11:18:56 -0400

    Add files via upload

--- Challenge/PyPoll_Challenge.py
+++ Challenge/PyPoll_Challenge.py
@@ -90,7 +90,7 @@ with open(file_to_save, "w") as txt_file:
         f"Total Votes: {total_votes:,}\n"
         f"-------------------------\n\n"
         f"County Votes:\n"
-        f"-------------------------\n")
+       )
     print(election_results, end="")
 
     txt_file.write(election_results)
